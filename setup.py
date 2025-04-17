febeb23774b0757c83758a0c60a6ba033e9f2e13
dloiolb <98446093+dloiolb@users.noreply.github.com>
2025-04-16 22:02:46 -0500

    fixed setup

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+def parse_requirements():
+    requirements = Path('requirements.txt').read_text().splitlines()
+    return requirements
 
 setup(
     name='simulation',
     version='0.1',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=parse_requirements(),
 )
